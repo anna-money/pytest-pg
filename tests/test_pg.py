307f02d533a8fe83f964432471b76eb2f5f4cdaf
Yury Pliner <yury.pliner@gmail.com>
2022-12-02 07:10:51 +0000

    Support postgresql 15

--- tests/test_pg.py
+++ tests/test_pg.py
@@ -2,20 +2,24 @@ import pytest_pg
 
 
 def test_pg(pg: pytest_pg.PG) -> None:
-    assert True
+    assert pg
 
 
 def test_pg_11(pg_11: pytest_pg.PG) -> None:
-    assert True
+    assert pg_11
 
 
 def test_pg_12(pg_12: pytest_pg.PG) -> None:
-    assert True
+    assert pg_12
 
 
 def test_pg_13(pg_13: pytest_pg.PG) -> None:
-    assert True
+    assert pg_13
 
 
 def test_pg_14(pg_14: pytest_pg.PG) -> None:
-    assert True
+    assert pg_14
+
+
+def test_pg_15(pg_15: pytest_pg.PG) -> None:
+    assert pg_15
